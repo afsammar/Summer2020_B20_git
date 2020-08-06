1eb32dba4b414add1dd3a74543e475cd40424754
afsammar <69063391+afsammar@users.noreply.github.com>
2020-08-06 17:17:05 -0400

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,5 +3,6 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Everything is fun");
+        System.out.println("Ammar");
     }
 }
